53742459dd1686c2f7045316df3ae45a2696465b
savoldelli <jeansavol@gmail.com>
2018-11-26 15:46:11 +0100

    othertest

---
+++ poneys/tests/OtherTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Classe de test de gestion de poneys
+ */
+class OtherTest extends TestCase
+{
+    public function teststubPoneyname(){
+        $this->assertEquals(1+1,2);
+    }
+}
+?>
